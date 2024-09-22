22f867ce67eb3508ffd789efc29ce466f761f379
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-09-22 21:35:25 +0500

    animation has been Added

--- script.js
+++ script.js
@@ -6,3 +6,37 @@ function raf(time) {
 }
 
 requestAnimationFrame(raf)
+
+document.querySelectorAll(".aspect-square").forEach(elem => {
+
+    let image = elem.querySelector("img");
+    let tl = gsap.timeline();
+
+    let xTransform = gsap.utils.random(-100, 100);
+
+    tl.set(image, {
+        transformOrigin: `${xTransform < 0 ? 0 : "100%"}`
+    }, "start")
+        .to(image, {
+            scale: 0,
+            ease: "none",
+            scrollTrigger: {
+                trigger: image,
+                start: "top center",
+                end: "bottom top",
+                scrub: true
+            }
+        }, "start")
+        .to(elem, {
+            xPercent: xTransform,
+            ease: "none",
+            scrollTrigger: {
+                trigger: image,
+                start: "top bottom",
+                end: "bottom top",
+                scrub: true
+            }
+
+        }, "start")
+
+})
